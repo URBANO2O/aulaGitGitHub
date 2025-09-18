4777e045afb152f13510eec26c6b900cbf220a52
Urbano Coutinho Neto <urbanoneto37@gmail.com>
2025-09-18 09:25:52 -0300

    Segundo commit do porjeto: somar os dois inteiros informados pelo usuario.

--- arq.js
+++ arq.js
@@ -1,9 +1,11 @@
 function soma {} {
-    var n1, n2;
-    
+    var n1, n2, soma;
+
     n1 = document.getElementById ("PrimeiroNumero").value; 
     n2 = document.getElementById ("SegundoNumero").value;
 
     n1 = parseInt (n1);
     n2 = parseInt (n2);
+
+    soma = n1 + n2;
 }
